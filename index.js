03f9ee5543b943a7cb65ff27d3c91c2c865ba8e4
Arina-Keys <dajagessar@gmail.com>
2024-02-28 11:35:41 +0100

    uuuhuhhhhhhuuhhhh

---
+++ index.js
@@ -0,0 +1,8 @@
+const names = [];
+
+function addImage(){
+    var img = document.createElement("img");
+    img.src = "https://www.the-sun.com/wp-content/uploads/sites/6/2023/10/www-instagram-com-monkeycatluna-hl-851711797.jpg?strip=all&w=960";
+var src = document.getElementById("img");
+src.appendChild(img);
+}
